8ed069452bbf69b8f6d7a0b69181cdde49c22243
Patrascu Hary <harypatrascu.1@gmail.com>
2024-12-23 20:59:55 +0200

    Added HeaderToImageConverter class that checks wheter a file is a drive, folder or file and gives it a corresponding image.

---
+++ WPF File Explorer/HeaderToImageConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace WPF_File_Explorer
+{
+    /// <summary>
+    /// Set image based on path(if it's a drive, folder or file)
+    /// </summary>
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    internal class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var path = (string)value;
+            var image = "Images/drive.png";
+            var name = Path.GetFileName(path);
+
+            if (path == null)
+                return null;
+
+            if (string.IsNullOrEmpty(name))
+                image = "Images/drive.png";
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "Images/folder_closed.png";
+            else
+                image = "Images/file.png";
+
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
